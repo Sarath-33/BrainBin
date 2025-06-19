91456d9295a9fdd256023f49cfc86a3451d116b3
Sarath-33 <withsarath.m@gmail.com>
2025-06-19 11:25:42 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,27 @@
 import express from "express";
 import dotenv from 'dotenv'
 import cors from "cors"
+import path from "path"
+
 
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config()
+
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 
 //middleware
-app.use(cors({
-    origin: "http://localhost:5173",
-}))
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }))
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -23,7 +30,17 @@ app.use(rateLimiter);
 // app.use((req,res,next)=>
 //     next()
 // })
-app.use("/api/notes", notesRoutes)
+app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+
+}
+
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`server is running on ${PORT}`);
